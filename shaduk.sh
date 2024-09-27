edc4460ab7c0df3372969587412cbc010bbb7c28
WEASTERNSMITH <98036437+WEASTERNSMITH@users.noreply.github.com>
2024-09-27 23:39:01 +0200

    Update shaduk.sh

--- shaduk.sh
+++ shaduk.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 apt update
-apt-get update -y && apt-get install wget curl sudo nano -y
+apt-get update -y && apt-get install wget curl sudo -y
 sudo su
 wget https://github.com/coder/code-server/releases/download/v4.12.0/code-server-4.12.0-linux-amd64.tar.gz
 tar -xf code-server-4.12.0-linux-amd64.tar.gz
@@ -10,8 +10,5 @@ sed -n '3'p ~/.config/code-server/config.yaml
 lt --port 12345 > localtunnel.log &
 rm -rf https://github.com/coder/code-server/releases/download/v4.12.0/code-server-4.12.0-linux-amd64.tar.gz
 cat localtunnel.log && wget https://github.com/WEASTERNSMITH/testlab1/raw/refs/heads/main/tail.sh && bash tail.sh
-while [ 1 ]; do
-sleep 3
-done
-sleep 999
+
 
