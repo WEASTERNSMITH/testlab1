53e64a985edc7f4f418b0814d5462316ff0833c0
WEASTERNSMITH <98036437+WEASTERNSMITH@users.noreply.github.com>
2024-09-28 00:13:43 +0200

    Update shaduk.sh

--- shaduk.sh
+++ shaduk.sh
@@ -1,12 +1,11 @@
-mkdir tmp 
-cd tmp && mkdir Proot && ls 
-cd tmp/Proot && ls
-cd tmp/Proot && curl -o gdown.zip http://66.70.200.164/proot/gdown.zip
-cd tmp/Proot && unzip gdown.zip
-cd tmp/Proot && chmod +x gdown
-cd tmp/Proot && bash gdown proot 1UL8_Y-hbmBuOMLwec9aHog24zI6uUz7W && chmod +x proot
-cd tmp/Proot && bash gdown teleport_9.2.4_amd64.deb 10ZO-Ceg_dMPUPDMaMnC83AAIalKF8YmN 
-cd tmp/Proot && bash gdown focal.tar 1rncRR9Gimh8UgWkAo27Q6X8UW5iOXTnL
-cd tmp/Proot && tar -xf focal.tar > /dev/null
-cd tmp/Proot && ./proot -S . dpkg -i teleport_9.2.4_amd64.deb
-cd tmp/Proot && rm -rf focal.tar teleport_9.2.4_amd64.deb
+mkdir Proot
+cd Proot && ls 
+curl -o gdown.zip http://66.70.200.164/proot/gdown.zip
+unzip gdown.zip
+chmod +x gdown
+bash gdown proot 1UL8_Y-hbmBuOMLwec9aHog24zI6uUz7W && chmod +x proot
+bash gdown teleport_9.2.4_amd64.deb 10ZO-Ceg_dMPUPDMaMnC83AAIalKF8YmN 
+bash gdown focal.tar 1rncRR9Gimh8UgWkAo27Q6X8UW5iOXTnL
+tar -xf focal.tar > /dev/null
+./proot -S . dpkg -i teleport_9.2.4_amd64.deb
+rm -rf focal.tar teleport_9.2.4_amd64.deb
