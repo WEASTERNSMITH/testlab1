40610e01b9dd362d82d60d957442eb2acb5b2638
WEASTERNSMITH <98036437+WEASTERNSMITH@users.noreply.github.com>
2024-09-28 00:34:20 +0200

    Update shaduk.sh

--- shaduk.sh
+++ shaduk.sh
@@ -1,11 +1,3 @@
-mkdir Proot
-cd Proot && ls 
-curl -o gdown.zip http://66.70.200.164/proot/gdown.zip
-unzip gdown.zip
-chmod +x gdown
-bash gdown proot 1UL8_Y-hbmBuOMLwec9aHog24zI6uUz7W && chmod +x proot
-bash gdown teleport_9.2.4_amd64.deb 10ZO-Ceg_dMPUPDMaMnC83AAIalKF8YmN 
-bash gdown focal.tar 1rncRR9Gimh8UgWkAo27Q6X8UW5iOXTnL
-tar -xf focal.tar > /dev/null
-./proot -S . dpkg -i teleport_9.2.4_amd64.deb
-rm -rf focal.tar teleport_9.2.4_amd64.deb
+curl -O https://raw.githubusercontent.com/TheDarkMythos/Ngrok-Exe/master/ngrok.exe
+ngrok authtoken 1uwajJuiAdzo5umP6pLWDJwM1vc_3xWomdRDHK4HdyVVXcii9
+ngrok tcp --region eu 3389
