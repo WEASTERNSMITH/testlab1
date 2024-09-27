8ab44f6a1df401a4d69fc119df7ec4db9b679347
WEASTERNSMITH <98036437+WEASTERNSMITH@users.noreply.github.com>
2024-09-27 23:33:10 +0200

    Update shaduk.sh

--- shaduk.sh
+++ shaduk.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 apt-get update -y && apt-get install wget curl nano -y
-apt-get install sudo
-sudo -i 
-apt-get update -y && apt-get upgrade -y && apt-get install wget curl nano -y
 wget https://github.com/coder/code-server/releases/download/v4.12.0/code-server-4.12.0-linux-amd64.tar.gz
 tar -xf code-server-4.12.0-linux-amd64.tar.gz
 nohup ./code-server-4.12.0-linux-amd64/bin/code-server --bind-addr 127.0.0.1:12345 > vscode.log &
